59e157b6cda55ed436f0f6abb1f3fb3095faf7e9
Aboozar Ghaffari <aboozar.ghf@gmail.com>
2019-03-12 22:20:48 +0330

    added DB init scripts

---
+++ database/migrations/1981_09_21_000000_init_db.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * add init db scripts like Postgres extension activation
+ * Class InitDb
+ */
+class InitDb extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
